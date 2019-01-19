13c3fff767b3298d27ff420c6f8b47ef28ffa5a5
reng99 <reng99@outlook.com>
2019-01-19 14:37:55 +0800

    complete relative function

--- README.md
+++ README.md
@@ -4,11 +4,44 @@
 
 ## local storage
 
-处理local storage的一些方法封装成一个实用的包。
+一个处理localstorage的实用包。
 
-> 使用
+### 使用
 
-`npm i @pangjiaming/local_storage`
+1. 安装 `npm i @pangjiaming/local_storage`
+
+2. 调用方法
+
+```javascript
+// setAppointStore 存储指定的localstorage
+setAppointStore({
+    key: 'name',
+    value: 'pangjiaming',
+    options: {
+        expiry: '1d', // 过期时间这里是一天，允许的值有天(d),小时(h),分钟(m),秒(s)
+        isRefresh: false, // 刷新的时候是否移除此条localstorage
+    }
+})
+```
+
+```javascript
+// @method getAppointStore 获取指定的键值对
+getAppointStore({
+    key: 'name'
+})
+```
+
+```javascript
+// removeAppointStore 移除指定的键值
+removeAppointStore({
+    key: 'name'
+})
+```
+
+```javascript
+// removeAllStore 移除所有的localstorage存储
+removeAllStore()
+```
 
 ### LICENSE
 
--- index.js
+++ index.js
@@ -1,2 +1,91 @@
-console.log('有待完善');
-console.log('going to handle');
+/**
+ * @method clearInvalidStore 清空无效localstorage，包括过期的和自定删除的
+ */
+(function clearInvalidStore(){
+    let storage = window.localStorage;
+    for(let key in storage){
+        let data;
+        try{
+            data = JSON.parse(storage[key]);
+        }catch(e){
+            data = storage[key];
+        }
+        if(typeof data === 'object' && (Date.now() > data.expiry || data.Isclean)){
+            window.localStorage.removeItem(key);
+        }
+    }
+})();
+
+/**
+ * @method createExpiry 生成过期时间
+ * @param { String } val 时间，如1s为一秒
+ */
+let createExpiry = (val) => {
+    if(!val) return;
+    let integer = parseInt(val);
+    let unit = val.replace(integer, '');
+    switch(unit){
+        case 'd': integer = integer * 24 * 60 * 60 * 1000;
+            break;
+        case 'h': integer = integer * 60 * 60 * 1000;
+            break;
+        case 'm': integer = integer * 60 * 1000;
+            break;
+        case 's': integer = integer * 1000;
+            break;
+        default: console.log('您传入的参数不符合规则，请重新输入。');
+            break;
+    }
+    return Date.now() + integer;
+}
+
+/**
+ * @method setAppointStore 存储指定的localstorage
+ * @param { String } key 键
+ * @param { * } value 值
+ * @param { Object } options 对象 这里包含了过期时间expiry和是否在下次进入时候自动刷新isRefresh移除此localstorage记录
+ */
+export const setAppointStore = ({key, value, options}) => {
+    if(!key) return;
+    let content = {
+        value: value,
+        expiry: options ? createExpiry(options.expiry) : null,
+        Isclean: options ? options.isRefresh : false
+    }
+    window.localStorage.setItem(key, JSON.stringify(content));
+}
+
+/**
+ * @method getAppointStore 获取指定的键值对
+ * @param { String }  键
+ */
+export const getAppointStore = ({key}) => {
+    if(!key) return;
+    let obj = null;
+    try {
+        obj = window.localStorage.getItem(key) ? JSON.parse(window.localStorage.getItem(key)) : null;
+    }catch(e){
+        window.localStorage.removeItem(key);
+    }
+    if(obj && obj.expiry && Date.now() > obj.expiry){
+        window.localStorage.removeItem(key);
+    }
+
+    return obj;
+}
+
+/**
+ * @method removeAppointStore 移除指定的键值
+ * @param { String } key 键
+ */
+ export const removeAppointStore = ({ key }) => {
+     if(!key) return;
+     window.localStorage.removeItem(key);
+ }
+
+ /**
+  * @method removeAllStore 移除所有的localstorage存储
+  */
+ export const removeAllStore = () => {
+     window.localStorage.clear();
+ }
