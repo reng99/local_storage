b67e77a7030494d988d46b62ef0331a01d1d3dac
reng99 <reng99@outlook.com>
2019-01-20 22:37:54 +0800

    添加压缩

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,8 +1,13 @@
+const uglify = require('rollup-plugin-uglify').uglify;
+
 module.exports = (options) => {
     const { input, fileName, name} = options;
     return {
         input: {
-            input
+            input,
+            plugins: [
+                uglify()
+            ]
         },
         output: {
             file: fileName,
