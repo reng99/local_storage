63826791a29d3d410843536cedea20325f3ccbf5
reng99 <reng99@outlook.com>
2019-01-20 17:47:19 +0800

    成功引入rollup

--- build/index.js
+++ build/index.js
@@ -1,1 +1,32 @@
-console.log("Build entry point");
+const rollup = require('rollup');
+const path = require('path');
+
+const inputOptions ={
+    input: path.join(__dirname, '../index.js')
+};
+
+const outputOptions ={
+    file: path.join(__dirname, '../dist/localstorage.js'),
+    format: "iife",
+    name: "r"
+};
+
+async function build() {
+    const bundle = await rollup.rollup(inputOptions);
+
+    console.log(bundle.watchFiles); 
+    const { output } = await bundle.generate(outputOptions);
+
+    for (const chunkOrAsset of output) {
+        if (chunkOrAsset.isAsset) {
+            console.log('Asset', chunkOrAsset);
+        } else {
+            console.log('Chunk', chunkOrAsset.modules);
+        }
+    }
+
+    // or write the bundle to disk
+    await bundle.write(outputOptions);
+}
+
+build();
---
+++ build/rollup.config.js
@@ -0,0 +1,12 @@
+// module.exports = () => {
+//     return {
+//         input: {
+//             input: "../index.js",
+//         },
+//         output: {
+//             file: "../dist/"+"localstorage.js",
+//             format: "iife",
+//             name: "localstorage.js"
+//         }
+//     };
+// };
